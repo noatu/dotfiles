3a8f55211f45cf48c4e70a7399e653fcea14a349
Anton Bilous <oxidate@tuta.io>
2025-06-26 21:40:43 +0300

    Helix

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -31,7 +31,7 @@ set -gx SCREENRECORD_DIR $XDG_VIDEOS_DIR/screen
 
 
 # Find an editor
-for e in nvim vim vi helix nano
+for e in helix nvim vim vi nano
     type -q $e; or continue
     set -gx EDITOR $e
     break
